5934181488ef3c09fc46a13d14ea15d00686acbd
Matthew Robertson <Matthew.Robertson@mi.mun.ca>
2025-04-22 13:00:16 -0230

    simplify model code and how it is run - remove options for alternative model structures

--- CreelCatch.cpp
+++ CreelCatch.cpp
@@ -5,18 +5,12 @@
 template<class Type>
 Type objective_function<Type>::operator() ()
 {
- 
- // Load data
+  
+  // Load data
   DATA_VECTOR(log_C); // Catch
   DATA_VECTOR(log_E); // Effort
-  DATA_VECTOR(first_covar); // Covariate
-  DATA_VECTOR(second_covar); // Covariate
-  DATA_VECTOR(third_covar); // Covariate
-  DATA_SCALAR(effort_switch); // Switch to determine effort model structure
-  DATA_SCALAR(catch_switch); // Switch to determine catch model structure
-  DATA_SCALAR(n_covars); // Scalar to define the number of covariates included
-  DATA_IVECTOR(region); // Integer that defines regions
-  DATA_IVECTOR(iGL); // Integer to denote great lakes waterbodies
+  DATA_VECTOR(wb_area); // Waterbody area
+  DATA_IVECTOR(region); //Region
   
   // Create object that is the length of the catch data
   int N = log_C.size();
@@ -41,7 +35,7 @@ Type objective_function<Type>::operator() ()
   using namespace density;
   Type nll = 0.0;  
   
-  int it, igl;
+  int it;
   
   vector<Type> log_pred_C = log(pred_C);
   vector<Type> log_pred_E = log(pred_E);
@@ -50,82 +44,16 @@ Type objective_function<Type>::operator() ()
   //effort model
   for(int n = 0;n < N;++n){ 
     it = region(n);
-    igl = iGL(n);
     
-    // Model without regional effects
-    if(effort_switch==0){
-      if(n_covars==0){
-        log_pred_E(n) = tau_int(0);
-      }
-      if(n_covars==1){
-        if(igl==0){
-          log_pred_E(n) = tau_int(0)+ tau(0)*first_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(0)+ tau(1)*first_covar(n);
-        }
-      }
-      if(n_covars==2){
-        if(igl==0){
-          log_pred_E(n) = tau_int(0)+ tau(0)*first_covar(n) + tau(2)*second_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(0)+ tau(1)*first_covar(n) + tau(2)*second_covar(n);
-        }
-      }
-      if(n_covars==3){
-        if(igl==0){
-          log_pred_E(n) = tau_int(0)+ tau(0)*first_covar(n) + tau(2)*second_covar(n)+ tau(3)*third_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(0)+ tau(1)*first_covar(n) + tau(2)*second_covar(n)+ tau(3)*third_covar(n);
-        };
-    }
-    }
-    
-    // Model with regional effects
-    if(effort_switch==1){
-      if(n_covars==0){
-        log_pred_E(n) = tau_int(it);
-      }
-      if(n_covars==1){
-        if(igl==0){
-        log_pred_E(n) = tau_int(it)+ tau(0)*first_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(it)+ tau(1)*first_covar(n);
-        }
-      }
-      if(n_covars==2){
-        if(igl==0){
-          log_pred_E(n) = tau_int(it)+ tau(0)*first_covar(n) + tau(2)*second_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(it)+ tau(1)*first_covar(n) + tau(2)*second_covar(n);
-        }
-      }
-      if(n_covars==3){
-        if(igl==0){
-          log_pred_E(n) = tau_int(it)+ tau(0)*first_covar(n) + tau(2)*second_covar(n)+ tau(3)*third_covar(n);
-        }
-        if(igl==1){
-          log_pred_E(n) = tau_int(it)+ tau(1)*first_covar(n) + tau(2)*second_covar(n)+ tau(3)*third_covar(n);
-        };
-      }
-    }
+    log_pred_E(n) = tau_int(it)+ tau(0)*wb_area(n);
     
     // Define effort model residuals
     E_resid(n) = log_E(n) - log_pred_E(n);
     std_resid_E(n) = E_resid(n)/sd(0);
     
     //catch model formulations
-    if(catch_switch==0){log_pred_C(n) = log_q(0) + slope(it) * log_pred_E(n);}
-    if(catch_switch==1){log_pred_C(n) = log_q(it) + slope(0) * log_pred_E(n);}
-    if(catch_switch==2){log_pred_C(n) = log_q(it) + log_pred_E(n);}
-    if(catch_switch==3){log_pred_C(n) = slope(0) * log_pred_E(n);}
-    if(catch_switch==4){log_pred_C(n) = log_q(0) + log_pred_E(n);}
-    if(catch_switch==5){log_pred_C(n) = log_q(0) + slope(0) * log_pred_E(n);}
-
+    log_pred_C(n) = log_q(0) + slope(0) * log_pred_E(n);
+    
     // Define catch model residuals
     C_resid(n) = log_C(n) - log_pred_C(n);
     std_resid_C(n) = C_resid(n)/sd(1);
--- Running_CreelCatch_Model.R
+++ Running_CreelCatch_Model.R
@@ -23,93 +23,59 @@ useable_dat<-subset(useable_dat, GL==0)
 # One waterbody in Minnesota that was Lake Superior but was spelled incorrectly so did not get captured by Great Lakes map
 useable_dat<-subset(useable_dat, WaterbodyID!="MN_1198")
 
-model_run<-list(
-  data_type = "2000", # Give info on any data subsets for Rdata naming
-  n_covars=1, # 0 is no covariates, 1 is one covariates, 2 is 2 covariates
-  first_covar=log(useable_dat$Area),
-  second_covar=useable_dat$Area,
-  third_covar=useable_dat$Area,
-  #### ONLY USED FOR BASIC MODEL
-  int_switch = 0, # 0 Means no intercept, #1 means intercept
-  ### ONLY USED FOR RE MODEL
-  effort_switch=1, # 0 is no random intercepts, 1 is random intercepts
-  catch_switch=5 # Switch for random effect formulation in the catch model
-)
-
-if(model_run$n_covars==0){
-  covars<-"None"
-}
-
 tmb.data<-list(
   log_C=log(useable_dat$cpd),
   log_E=log(useable_dat$epd),
-  first_covar=model_run$first_covar,
-  second_covar=model_run$second_covar,
-  third_covar=model_run$third_covar,
-  effort_switch=model_run$effort_switch, 
-  catch_switch=model_run$catch_switch, 
-  n_covars=model_run$n_covars,
-  region = useable_dat$istate,
-  iGL = useable_dat$GL
+  wb_area=log(useable_dat$Area),
+  region = useable_dat$istate
 )
-
-# Automation code to set-up data/parameters appropriately depending on desired model structure
-# Effort intercept 
-if(tmb.data$effort_switch==0){ tau_int=1} 
-if(tmb.data$effort_switch==1){ tau_int = rep(1, length(unique(useable_dat$istate)))}
-
-# Q modeling
-if(tmb.data$catch_switch<3 & tmb.data$catch_switch>0){q_dev = rep(1, length(unique(useable_dat$istate)))}  
-if(tmb.data$catch_switch>2){q_dev = 0.1}
-if(tmb.data$catch_switch==0){q_dev = 0.1}
-
-# Slope modeling
-if(tmb.data$catch_switch==0){slope=rep(1,  length(unique(useable_dat$istate)))} 
-if(tmb.data$catch_switch>0){slope = 1}
-
-# Number of covariates
-if(tmb.data$n_covars==0){tau=rep(1,1)}
-if(tmb.data$n_covars==1){tau=rep(1,2)}
-if(tmb.data$n_covars==2){tau=rep(1,3)}
-if(tmb.data$n_covars==3){tau=rep(1,4)}
 
 # Parameter definitions
 parameters<-list(
-  slope=slope,
-  q_dev = q_dev,
-  tau_int = tau_int,
-  tau = tau,
+  slope= 1,
+  q_dev = 0.1,
+  tau_int = rep(1, length(unique(useable_dat$istate))),
+  tau = 1,
   log_sd = rep(log(2),2)
 )
 
-# Naming the random effects
-rname_temp = na.omit(c(ifelse(tmb.data$catch_switch==0,  "slope" ,NA),
-                       ifelse(tmb.data$catch_switch<3,  "q_dev" ,NA),
-                       ifelse(tmb.data$effort_switch==1,  "tau_int" ,NA)))
-rname<-NULL
-if(length(rname_temp)!=0)rname<-rname_temp
-
-# Mapping out parameters depending on model formulation
-map<-list(
-  tau=factor(c("1",NA))
+rname = c(
+  "tau_int"
 )
 
 # Load cpp template
 dyn.unload("CreelCatch")
 TMB::compile("CreelCatch.cpp")
-dyn.load("CreelCatch")
+dyn.load(dynlib("CreelCatch"))
 
 # Define data and parameters within the template
-obj <- MakeADFun(tmb.data,parameters,map=map,random=rname,
+obj <- MakeADFun(tmb.data,parameters,random=rname,
                  DLL="CreelCatch",inner.control=list(maxit=500,trace=F))
 
 # Run the model
 opt<-nlminb(obj$par,obj$fn,obj$gr,
             control = list(trace=10,eval.max=2000,iter.max=1000))
 
+2*opt$objective+ 2*length(opt$par) # AIC
+
 # Get model output
 rep = obj$report()
 sdrep<-sdreport(obj)
+
+
+# Calculate R^2 based on Nakagawa and Schielzeth (2013)
+fe_var_E<-var(rep$tau*tmb.data$wb_area) #fixed effects variance from effort model
+fe_var_C<-var(rep$log_q + rep$slope*rep$log_pred_E) #fixed effects variance from catch model
+
+u_hat <- obj$env$last.par[3:9] # extract random effects from effort model
+re_var <- var(u_hat) #calculate variance of random effects
+
+var_resid_E<-rep$sd[1] #residual variance from effort model
+var_resid_C<-rep$sd[2] #residual variance from catch model
+
+conditional_r2_E<-(fe_var_E+re_var)/(fe_var_E+re_var+var_resid_E) #conditional r2 from effort model
+marginal_r2_C<-(fe_var_C)/(fe_var_C+var_resid_C) #marginal r2 from catch model (no random effects)
+
 
 # Check parameter estimates and CIs
 sdrep$value+qnorm(0.975)*sdrep$sd
@@ -130,7 +96,7 @@ ce_dat<-data.frame(log_E=tmb.data$log_E, log_C=tmb.data$log_C)
 p1<-ggplot()+
   geom_ribbon(data=pred_c_e, aes(ymin=pred_C_low, ymax=pred_C_high, x=pred_E), fill="lightblue", alpha=0.5)+
   geom_point(data=ce_dat, aes(x=log_E, y=log_C), col="darkgrey")+
-  geom_line(data=pred_c_e, aes(x=pred_E, y=pred_C), size=1.5)+#xlim(0,10)+
+  geom_line(data=pred_c_e, aes(x=pred_E, y=pred_C), linewidth=1.5)+#xlim(0,10)+
   xlab("log(Effort Hours per Day)")+ylab("log(Catch Numbers per Day)")+
   theme_bw()+
   theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
@@ -143,12 +109,12 @@ pred_area_low<-mean(rep$tau_int) + (rep$tau[1]-qnorm(0.975)*sdrep$sd[3])*pred_wb
 pred_area_high<-mean(rep$tau_int) + (rep$tau[1]+qnorm(0.975)*sdrep$sd[3])*pred_wb_area
 
 pred_area_df<-data.frame(pred_wb_area=pred_wb_area,pred_area=pred_area, pred_area_low=pred_area_low, pred_area_high=pred_area_high)
-area_dat<-data.frame(log_E=tmb.data$log_E, area=tmb.data$first_covar)
+area_dat<-data.frame(log_E=tmb.data$log_E, area=tmb.data$wb_area)
 
 p2<-ggplot()+
   geom_ribbon(data=pred_area_df, aes(ymin=pred_area_low, ymax=pred_area_high, x=pred_wb_area), fill="lightblue", alpha=0.5)+
   geom_point(data=area_dat, aes(y=log_E, x=area), col="darkgrey")+
-  geom_line(data=pred_area_df, aes(x=pred_wb_area, y=pred_area), size=1.5)+#xlim(0,10)+
+  geom_line(data=pred_area_df, aes(x=pred_wb_area, y=pred_area), linewidth=1.5)+
   ylab("log(Effort Hours per Day)")+xlab(bquote(bold('Waterbody Area log'(km^2))))+
   theme_bw()+
   theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
@@ -177,5 +143,155 @@ ggarrange(p1,ggarrange(p2, p3, ncol = 2, labels=c("B","C")),
           nrow = 2, labels="A"
 ) 
 dev.off()
+
+
+# Effort residual plots -------------------------------------------------------
+
+effort_resids<-data.frame(resids= rep$E_resid, std_resids = rep$std_resid_E,
+                          region = tmb.data$region, wb_area = tmb.data$wb_area,
+                          effort_dat = tmb.data$log_E, effort_pred = rep$log_pred_E,
+                          survey_time = useable_dat$Duration)
+
+jpeg("effort_resid_hist.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=effort_resids, aes(x=resids))+
+  geom_histogram(colour="black",fill="lightblue")+
+  geom_vline(xintercept=0, col="red", linetype="dashed")+ 
+  theme_bw()+xlab("Effort Residuals")+ylab("Count")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("effort_qq.jpeg", width=6,height=4, units="in", res=400)
+qqnorm(effort_resids$resids, pch=19)
+qqline(effort_resids$resids, col="red")
+dev.off()
+
+jpeg("effort_obs_pred.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=effort_resids, aes(x=effort_dat, y=effort_pred))+
+  geom_point(colour="black",fill="lightblue")+
+  coord_cartesian(xlim=c(-1,11),ylim=c(-1,11))+
+  geom_abline(intercept=0, slope=1, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Effort) Observations")+ylab("log(Effort) Predictions")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("effort_resid_pred.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=effort_resids, aes(x=effort_pred, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Effort) Predictions")+ylab("Standardized Effort Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("effort_resid_wb.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=effort_resids, aes(x=wb_area, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Waterbody Area)")+ylab("Standardized Effort Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+effort_resids$region<-factor(effort_resids$region, 
+                             labels=c("Midwest","Northeast","Northern Great Plains","Southeast","Southwest","Southern Great Plains","Northwest"))
+
+jpeg("effort_resid_wb_region.jpeg", width=7,height=7, units="in", res=400)
+ggplot(data=effort_resids, aes(x=wb_area, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Waterbody Area)")+ylab("Standardized Effort Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))+
+  facet_wrap(~region)
+dev.off()
+
+jpeg("effort_resid_surv_duration.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=effort_resids, aes(x=survey_time, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=2)+ 
+  theme_bw()+xlab("Survey Duration (days)")+ylab("Standardized Effort Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+# Catch residual plots -------------------------------------------------------
+
+catch_resids<-data.frame(resids= rep$C_resid, std_resids = rep$std_resid_C,
+                         region = tmb.data$region, wb_area = tmb.data$wb_area,
+                         catch_dat = tmb.data$log_C, catch_pred = rep$log_pred_C,
+                         survey_time = useable_dat$Duration,
+                         effort_pred = rep$log_pred_E)
+
+jpeg("catch_resid_hist.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=resids))+
+  geom_histogram(colour="black",fill="lightblue")+
+  geom_vline(xintercept=0, col="red", linetype="dashed")+ 
+  theme_bw()+xlab("Residuals")+ylab("Count")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("catch_qq.jpeg", width=6,height=4, units="in", res=400)
+qqnorm(catch_resids$resids, pch=19)
+qqline(catch_resids$resids, col="red")
+dev.off()
+
+jpeg("catch_obs_pred.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=catch_dat, y=catch_pred))+
+  geom_point(colour="black",fill="lightblue")+
+  coord_cartesian(xlim=c(-1,11),ylim=c(-1,11))+
+  geom_abline(intercept=0, slope=1, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Catch) Observations")+ylab("log(Catch) Predictions")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("catch_resid_pred.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=catch_pred, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Catch) Predictions")+ylab("Standardized Catch Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("catch_resid_wb.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=wb_area, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Waterbody Area)")+ylab("Standardized Catch Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+catch_resids$region<-factor(catch_resids$region, 
+                            labels=c("Midwest","Northeast","Northern Great Plains","Southeast","Southwest","Southern Great Plains","Northwest"))
+
+jpeg("catch_resid_wb_region.jpeg", width=7,height=7, units="in", res=400)
+ggplot(data=catch_resids, aes(x=wb_area, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Waterbody Area)")+ylab("Standardized Catch Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))+
+  facet_wrap(~region)
+dev.off()
+
+jpeg("catch_resid_effort.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=effort_pred, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("log(Effort) Predictions")+ylab("Standardized Catch Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
+jpeg("catch_resid_surv_duration.jpeg", width=6,height=4, units="in", res=400)
+ggplot(data=catch_resids, aes(x=survey_time, y=std_resids))+
+  geom_point(colour="black",fill="lightblue")+
+  geom_smooth(se=FALSE)+
+  geom_hline(yintercept=0, col="red", linetype="dashed", linewidth=1)+ 
+  theme_bw()+xlab("Survey Duration (days)")+ylab("Standardized Catch Residuals")+
+  theme(axis.text= element_text(size=14),axis.title=element_text(size=16,face="bold"))
+dev.off()
+
 
 save(rep, file="Model_output.RData")
